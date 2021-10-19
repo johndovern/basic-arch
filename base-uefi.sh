702490ec8a9176909d129df1250cdf1ad3096db5
johndovern <johndovern@tutamail.com>
2021-10-18 21:05:20 -0700

    update

--- base-uefi.sh
+++ base-uefi.sh
@@ -10,12 +10,12 @@ echo "arch" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
-echo root:[passhere] | chpasswd
+#echo root:[passhere] | chpasswd
 
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 
-pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g
+pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils bash-completion openssh rsync acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g
 
 # pacman -S --noconfirm xf86-video-amdgpu
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
@@ -28,22 +28,26 @@ systemctl enable bluetooth
 systemctl enable cups.service
 systemctl enable sshd
 systemctl enable avahi-daemon
-systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
 systemctl enable libvirtd
 systemctl enable firewalld
 systemctl enable acpid
 
+# Enable bat threshold for laptops (check for compatability between tlp and your machine)
+sed -i '501s/.//' /etc/tlp.conf
+sed -i '502s/.//' /etc/tlp.conf
+sed -i '508s/.//' /etc/tlp.conf
+sed -i '509s/.//' /etc/tlp.conf
+
+# Enable tlp, should be done after editing conf file to ensure settings are applied
+systemctl enable tlp
+
 #useradd -m [user]
 #echo [user]:[password] | chpasswd
 #usermod -aG libvirt [user]
 
-#echo "ermanno ALL=(ALL) ALL" >> /etc/sudoers.d/ermanno
+#echo "[user] ALL=(ALL) ALL" >> /etc/sudoers.d/ermanno
 
 
 printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
-
-
-
-
